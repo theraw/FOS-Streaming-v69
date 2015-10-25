8ae9c663f6d8406e82b0be90244ceb2eb90048d4
zgelici <zgelici@gmail.com>
2015-10-26 00:08:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,14 @@
 <?php
 include('_start.php');
-
 $databasemanagar->addConnection([
     'driver'    => 'mysql',
     'host'      => 'localhost',
-    'database'  => 'fos',
-    'username'  => 'root',
-    'password'  => 'zadik',
+    'database'  => 'xxx',
+    'username'  => 'ttt',
+    'password'  => 'zzz',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
 ]);
-
 $debug = false;
 include('_load.php');
