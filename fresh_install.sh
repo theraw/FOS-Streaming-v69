66947d7c8d4dbc3bcaad92af29aa52ce2f3f8e3e
cgheonea <ccgheonea@gmail.com>
2016-02-29 23:51:02 +0200

    Fix another surce.

--- fresh_install.sh
+++ fresh_install.sh
@@ -8,6 +8,6 @@ echo "Upgrading existing packages and removing the obsolete ones!"
 apt-get upgrade -y && apt-get autoremove -y
 
 cd /tmp
-wget -q https://github.com/zgelici/FOS-Streaming-v1/blob/master/install_panel.php -O install_panel.php && php install_panel.php
+wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/install_panel.php -O install_panel.php && php install_panel.php
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/my.cnf -O my.cnf
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/install_panel.php -O db_install.sh && chmod 755 db_install.sh && ./db_install.sh
