b1d0a1c84ed79aa66946c5e01618c58ff49db299
cgheonea <ccgheonea@gmail.com>
2016-02-29 23:31:18 +0200

    my.cnf - Add sql tweaked configuration file. db_install.sh - Remove existing MySQL server and install latest version. Few more improvements.

--- db_install.sh
+++ db_install.sh
@@ -14,8 +14,11 @@ echo "phpmyadmin phpmyadmin/mysql/admin-pass password $sqlpasswd" | debconf-set-
 echo "phpmyadmin phpmyadmin/mysql/app-pass password $sqlpasswd" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
 
-apt-get install -y mysql-server > /dev/null 2>&1
+apt-get install -y mysql-server-5.6 > /dev/null 2>&1
 apt-get install -y php5-mysql  > /dev/null 2>&1
+service mysql stop
+mv /tmp/my.cnf /etc/mysql/my.cnf
+service mysql start
 
 mysql -uroot -p$sqlpasswd -e "CREATE DATABASE $sqldatabase"
 mysql -uroot -p$sqlpasswd -e "grant all privileges on $sqldatabase.* to '$sqluname'@'localhost' identified by '$sqlpasswd'"
--- fresh_install.sh
+++ fresh_install.sh
@@ -4,6 +4,10 @@ echo "Done apt-get"
 echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl zip unzip
 echo "Done"
+echo "Upgrading existing packages and removing the obsolete ones!"
+apt-get upgrade -y && apt-get autoremove -y
+
 cd /tmp
 wget -q https://github.com/zgelici/FOS-Streaming-v1/blob/master/install_panel.php -O install_panel.php && php install_panel.php
+wget -q https://github.com/zgelici/FOS-Streaming-v1/blob/master/my.cnf -O my.cnf
 wget -q https://github.com/zgelici/FOS-Streaming-v1/blob/master/db_install.sh -O db_install.sh && chmod 755 db_install.sh && ./db_install.sh
