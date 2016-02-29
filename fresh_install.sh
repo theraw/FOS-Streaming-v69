8e02363cadaac3352ea328edf93c6fdcf7880609
cgheonea <ccgheonea@gmail.com>
2016-02-29 23:56:17 +0200

    Clean tmp before fresh installation.

--- fresh_install.sh
+++ fresh_install.sh
@@ -8,6 +8,7 @@ echo "Upgrading existing packages and removing the obsolete ones!"
 apt-get upgrade -y && apt-get autoremove -y
 
 cd /tmp
+rm -rf /tmp/
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/install_panel.php -O install_panel.php && php install_panel.php
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/my.cnf -O my.cnf
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/install_panel.php -O db_install.sh && chmod 755 db_install.sh && ./db_install.sh
