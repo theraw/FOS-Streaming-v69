86a21208064a8a9f49432308e360004812301ace
cgheonea <ccgheonea@gmail.com>
2016-02-29 23:13:47 +0200

    Some slight changes.

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,8 +5,5 @@ echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl zip unzip
 echo "Done"
 cd /tmp
-wget https://github.com/zgelici/FOS-Streaming-v1/blob/master/install_panel.php -O install_panel.php
-php install_panel.php
-wget https://github.com/zgelici/FOS-Streaming-v1/blob/master/db_install.sh
-chmod 755 db_install.sh
-./db_install.sh
+wget -q https://github.com/zgelici/FOS-Streaming-v1/blob/master/install_panel.php -O install_panel.php && php install_panel.php
+wget -q https://github.com/zgelici/FOS-Streaming-v1/blob/master/db_install.sh -O db_install.sh && chmod 755 db_install.sh && ./db_install.sh
