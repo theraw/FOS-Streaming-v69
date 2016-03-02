291ae272326fdeaf9ad720e047da566f90befc44
cgheonea <ccgheonea@gmail.com>
2016-03-03 01:18:57 +0200

    removing unattended mysql installation

--- db_install.sh
+++ db_install.sh
@@ -3,18 +3,6 @@
 
 echo  "Database Installation"
 
-read -p "Choose your MySQL database name: " sqldatabase
-read -p "Enter your MySQL username(usual 'root'): " sqluname 
-read -rep $'Enter your MySQL password (ENTER for none):' sqlpasswd
-
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $sqlpasswd'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $sqlpasswd'
-echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/app-password-confirm password $sqlpasswd" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-pass password $sqlpasswd" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/app-pass password $sqlpasswd" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
-
 UP=$(pgrep mysql | wc -l);
 if [ "$UP" -ne 1 ];
 then
@@ -28,6 +16,10 @@ apt-get install -y -q mysql-server mysql-common php5-mysql
 fi
     
 fi
+
+read -p "Choose your MySQL database name: " sqldatabase
+read -p "Enter your MySQL username(usual 'root'): " sqluname 
+read -rep $'Enter your MySQL password (ENTER for none):' sqlpasswd
 
 mysql -uroot -p$sqlpasswd -e "CREATE DATABASE $sqldatabase"
 mysql -uroot -p$sqlpasswd -e "grant all privileges on $sqldatabase.* to '$sqluname'@'localhost' identified by '$sqlpasswd'"
