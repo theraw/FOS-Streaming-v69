76afb35ed68f05e60aaa05384ae280366510813b
Cristian Gheonea <ccgheonea@gmail.com>
2016-03-02 17:51:28 +0200

    added libfaac-dev without it won't compile

--- ffmpeg_install.sh
+++ ffmpeg_install.sh
@@ -5,7 +5,7 @@ echo "Installing prerequisites"
 sudo apt-get update
 sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
   libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-  libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev
+  libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev libfaac-dev
 }
 
 #Compile yasm
