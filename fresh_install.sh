e1761ff65b4376cc5e79606acf5feafcca2eddb4
cgheonea <ccgheonea@gmail.com>
2016-03-02 09:55:02 +0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-apt-get update
-echo "Done apt-get"
-echo "Installing PHP5-CLI CURL"
-echo "Upgrading existing packages and removing the obsolete ones!"
-apt-get upgrade -y && apt-get install -f && apt-get autoremove && apt-get autoclean -y > /dev/null
+echo "Updating sources"
+apt-get update > /dev/null
+echo "Done"
 echo "Installing FOS Prerequisites..."
-apt-get install php5-cli curl zip unzip sudo nano dialog apt-utils python-software-properties apt nscd lsb-release -y > /dev/null
+apt-get install php5-cli php5-curl curl zip unzip sudo nano dialog apt-utils python-software-properties apt lsb-release -y > /dev/null
 echo "Done"
 
 
-chmod 777 /tmp
+chmod 777 /tmp > /dev/null
 cd /tmp > /dev/null
 rm -rf /tmp/* > /dev/null
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/install_panel.php -O install_panel.php > /dev/null
-php install_panel.php > /dev/null
-wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/my.cnf -O /etc/mysql/my.cnf.replace > /dev/null
+php install_panel.php
 wget -q https://raw.githubusercontent.com/zgelici/FOS-Streaming-v1/master/db_install.sh -O db_install.sh > /dev/null 
 chmod 755 db_install.sh > /dev/null
-./db_install.sh > /dev/null
+./db_install.sh
