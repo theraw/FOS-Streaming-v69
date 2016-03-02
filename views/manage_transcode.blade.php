a95bb7db2fdeab37b6bdf10d58618b630c907503
cgheonea <ccgheonea@gmail.com>
2016-03-03 01:59:31 +0200

    use libfaac instead.

--- views/manage_transcode.blade.php
+++ views/manage_transcode.blade.php
@@ -87,6 +87,7 @@
                                         </select>
                                     </div>
                                 </div>
+                                
 
                                 <div class="form-group">
                                     <label class="control-label col-md-3 col-sm-3 col-xs-12">Profile</label>
