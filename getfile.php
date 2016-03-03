74978afec059a7930de05fbce75d4f4fc16530c2
Cristian Gheonea <ccgheonea@gmail.com>
2016-03-03 15:39:19 +0200

    bouquets contain now categories and username in the name of the file

--- getfile.php
+++ getfile.php
@@ -15,7 +15,7 @@ $setting = Setting::first();
 if (isset($_GET['e2'])) {
     echo "#NAME FOS-Streaming \r\n";
     foreach ($user->categories as $category) {
-        echo "#DESCRIPTION ---###{$category}###---\r\n";
+        echo "#DESCRIPTION ---###{$category->name}###---\r\n";
         foreach ($category->streams as $stream) {
             if ($stream->running == 1) {
                 echo "#SERVICE 1:0:1:0:0:0:0:0:0:0:http%3A//" . $setting->webip . "%3A" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
@@ -37,7 +37,7 @@ if (isset($_GET['m3u'])) {
     echo "#EXTM3U \r\n";
     foreach ($user->categories as $category) {
         foreach ($category->streams as $stream) {
-
+            echo "#EXTINF:0," . $category->name . "\r\n";
             if ($stream->running == 1) {
                 echo "#EXTINF:0," . $stream->name . "\r\n";
                 echo "http://" . $setting->webip . ":" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
@@ -45,7 +45,7 @@ if (isset($_GET['m3u'])) {
         }
     }
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="tv_user.m3u"');
+    header('Content-Disposition: attachment; filename="' . $user->username . '_playlist.m3u"');
     header("Content-Transfer-Encoding: binary");
     header('Pragma: no-cache');
     header('Expires: 0');
@@ -63,7 +63,7 @@ if (isset($_GET['tv'])) {
     }
 
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="Channels.txt"');
+    header('Content-Disposition: attachment; filename="' . $user->username . '_tv.txt"');
     header("Content-Transfer-Encoding: binary");
     header('Pragma: no-cache');
     header('Expires: 0');
