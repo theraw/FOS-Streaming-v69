e8bb3cf996f03c59d30de3c86f4a3ec4828388e9
zadtrix <zgelici@gmail.com>
2017-02-14 23:10:35 +0100

    stream crash fix

--- fix_conf_files.php
+++ fix_conf_files.php
@@ -2,7 +2,7 @@
 $cpCommands = [
     "improvement/nginx.conf /home/fos-streaming/fos/nginx/conf/nginx.conf",
     "improvement/php-fpm.conf /home/fos-streaming/fos/php/etc/php-fpm.conf",
-    "improvement/www.conf /home/fos-streaming/php/fos/etc/pool.d/www.conf",
+    "improvement/www.conf /home/fos-streaming/fos/php/etc/pool.d/www.conf",
     "improvement/balance.conf /home/fos-streaming/fos/nginx/balance.conf"
 ];
 foreach($cpCommands as $command) {
